dbafe89ea1c56cf0b484c4d674469474617899f6
angelo ferretti <31105678+angiof@users.noreply.github.com>
2022-07-05 21:37:43 +0200

    init lib for git passy core

--- PassyCoreFeatures/src/main/java/com/example/passycorefeatures/Utils.kt
+++ PassyCoreFeatures/src/main/java/com/example/passycorefeatures/Utils.kt
@@ -3,7 +3,7 @@ package com.example.passycorefeatures
 import android.content.Context
 import android.widget.Toast
 
-class Utils {
+class UtilsXX {
     inner class Toasts(val ctx :Context){
         fun getToasts(){
             Toast.makeText(ctx, "tostis dalla lib ", Toast.LENGTH_SHORT).show()
